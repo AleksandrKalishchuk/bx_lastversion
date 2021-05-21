b86017c522254bb57de9d5582be4164cf4a3be70
Aleksandr Kalishchuk <50943310+AleksandrKalishchuk@users.noreply.github.com>
2021-05-21 10:54:46 +0300

    Fix
    Добавил еще exclude и поправил создание архива

--- make_lv.sh
+++ make_lv.sh
@@ -2,17 +2,18 @@
 
 # Create .last_version.zip
 # Author Shkabko R.
-# Repo 
+# Repo
 
 echo "Удаляем папку dist и старую сборку"
 rm -r dist
 rm -r .last_version.zip
 
 echo "Копируем файлы"
-rsync -av --progress ./ dist/ --exclude dist/ --exclude fx_bx_mc.sh --exclude .git/ --exclude .DS_Store --exclude make_lv.sh
+mkdir dist
+rsync -av --progress ./ dist/.last_version --exclude dist/ --exclude fx_bx_mc.sh --exclude .git/ --exclude .DS_Store --exclude make_lv.sh --exclude .idea --exclude .gitignore --exclude README.md
 
+cd dist/
 echo "Конвертируем файлы с utf-8 to windows-1251 $file"
-cd dist
 find ./ -name "*.php" -o -name "*.html" -o -name "*.css" -o -name "*.js"  -type f |
 while read file
 do
@@ -23,6 +24,6 @@ do
 done
 
 echo "Архивируем папку"
-zip -r ../.last_version.zip *
+zip -r ../.last_version.zip .last_version
 
 cd ../; rm -r dist
